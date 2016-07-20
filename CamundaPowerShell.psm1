ac0a0c239177b28c29b3670d42f6ca635fe34357
ChrisMagnuson <hourglasssand@gmail.com>
2016-07-20 13:50:23 -0400

    TaskFailure, Retries, APIFunctionWithQueryStringParameters, added process engine variable types, camunda server from environment variable

--- CamundaPowerShell.psm1
+++ CamundaPowerShell.psm1
@@ -1,40 +1,52 @@
 ﻿#Requires -Version 5
 
-$CamundaServer = "LocalHost"
-$CamundaRestEngineUri = "http://$($CamundaServer):8080/engine-rest/"
+if($env:CamundaServer) {
+    $Script:CamundaServer = $env:CamundaServer
+} else {
+    $Script:CamundaServer = "LocalHost"
+}
+
+$Script:CamundaRestEngineUri = "http://$($CamundaServer):8080/engine-rest"
+
+function Set-CamundaServer {
+    param(
+        $Name
+    )
+    $Script:CamundaServer = $Name
+}
 
 Function Get-CamundaProcessInstanceList {
     param(
-        $processInstanceIds,
-        $businessKey,
-        $caseInstanceId,
-        $processDefinitionId,
-        $processDefinitionKey,
-        $deploymentId,
-        $superProcessInstance,
-        $subProcessInstance,
-        $superCaseInstance,
-        $subCaseInstance,
-        $active,
-        $suspended,
-        $incidentId,
-        $incidentType,
-        $incidentMessage,
-        $incidentMessageLike,
-        $tenantIdIn,
-        $withoutTenantId,
-        $activityIdIn,
-        $variables,
-        $sortBy,
-        $sortOrder,
-        $firstResult,
-        $maxResults
+        #$processInstanceIds,
+        #$businessKey,
+        #$caseInstanceId,
+        #$processDefinitionId,
+        #$processDefinitionKey,
+        #$deploymentId,
+        #$superProcessInstance,
+        #$subProcessInstance,
+        #$superCaseInstance,
+        #$subCaseInstance,
+        #$active,
+        #$suspended,
+        #$incidentId,
+        #$incidentType,
+        #$incidentMessage,
+        #$incidentMessageLike,
+        #$tenantIdIn,
+        #$withoutTenantId,
+        #$activityIdIn,
+        #$variables,
+        #$sortBy,
+        #$sortOrder,
+        #$firstResult,
+        #$maxResults
     )
-    $QueryStringParameters = $PSBoundParameters.Keys |
-    where {$_ -ne "CustomFields"} | 
-    % { 
-        "$_=$([Uri]::EscapeDataString($PSBoundParameters[$_]))"
-    }
+    #$QueryStringParameters = $PSBoundParameters.Keys |
+    #where {$_ -ne "CustomFields"} | 
+    #% { 
+    #    "$_=$([Uri]::EscapeDataString($PSBoundParameters[$_]))"
+    #}
 
     Invoke-CamundaRestAPIFunction -MethodPath "/process-instance" -MethodHttpVerb GET
 }
@@ -55,6 +67,35 @@ function Remove-CamundaDeployments {
     foreach ($Deployment in $Deployments) {
         Invoke-WebRequest -Method Delete -Uri $($Uri + $Deployment.id + "?cascade=true")
     }
+}
+
+function Get-CamundaExternalTask {
+    param(
+        $ExternalTaskId,
+        $topicName,
+        $workerId,
+        $locked,
+        $notLocked,
+        $withRetriesLeft,
+        $noRetriesLeft,
+        $lockExpirationAfter,
+        $lockExpirationBefore,
+        $activityId,
+        $executionId,
+        $processInstanceId,
+        $processDefinitionId,
+        $tenantIdIn,
+        $active,
+        $priorityHigherThanOrEquals,
+        $priorityLowerThanOrEquals,
+        $suspended,
+        $sortBy,
+        $sortOrder,
+        $firstResult,
+        $maxResults
+    )
+
+    Invoke-CamundaRestAPIFunctionWithQueryStringParameters -MethodPath "/external-task" -MethodHttpVerb Get -Parameters $PSBoundParameters
 }
 
 function Get-CamundaExternalTasksAndLock {
@@ -97,7 +138,7 @@ function New-CamundaVariable {
         [parameter(Mandatory)]$Name,
         [parameter(Mandatory)]$Value,
         #https://docs.camunda.org/manual/7.5/user-guide/process-engine/variables/#supported-variable-values
-        [ValidateSet("boolean","bytes","short","integer","long","double","date","string","null","file")]$Type,
+        [ValidateSet("boolean","bytes","short","integer","long","double","date","string","null","file","JSON","XML")]$Type,
         [Parameter(ParameterSetName="Object")]$ObjectTypeName,
         [Parameter(ParameterSetName="Object")]$SerializationDataFormat,
         [Parameter(ParameterSetName="File")]$Filename,
@@ -142,7 +183,62 @@ function Invoke-CamundaRestAPIFunction {
         [parameter(Mandatory)]$MethodHttpVerb,
         $Body
     )
-    Invoke-WebRequest -Uri $("http://cmagnuson-lt:8080/engine-rest" + $MethodPath) -Method $MethodHttpVerb -Body $Body -Verbose -ContentType "application/json" |
+    Invoke-WebRequest -Uri $($Script:CamundaRestEngineUri + $MethodPath) -Method $MethodHttpVerb -Body $Body -Verbose -ContentType "application/json" |
     select -ExpandProperty content |
     ConvertFrom-Json
+}
+
+function Invoke-CamundaRestAPIFunctionWithQueryStringParameters {
+    param(
+        [parameter(Mandatory)]$MethodPath,
+        [parameter(Mandatory)]$MethodHttpVerb,
+        $Parameters = @{}
+    )
+    $URLEncodedParameters = @{}
+    $Parameters.Keys | % { $URLEncodedParameters.Add($_, [Uri]::EscapeDataString($Parameters[$_])) }
+
+    $URLEncodedParametersAsQueryString = $($URLEncodedParameters.Keys | % { $_ +"=" + $URLEncodedParameters[$_] }) -join "&"
+
+    $Result = Invoke-WebRequest -Uri $($Script:CamundaRestEngineUri + $MethodPath + "?" + $URLEncodedParametersAsQueryString) -Method $MethodHttpVerb -Verbose -ContentType "application/json" |
+    select -ExpandProperty content |
+    ConvertFrom-Json
+
+    $Result
+
+    #if ($Result.count -gt 1) {$Result[0]} else {$Result}
+}
+
+function Invoke-CamundaExternalTaskFailure {
+    param(
+        [parameter(Mandatory)]$ExternalTaskID,
+        [parameter(Mandatory)]$WorkerID,
+        [parameter(Mandatory)]$ErrorMessage,
+        $Retries = 0,
+        $RetryTimeout
+    )
+    
+    $CamundaExternalTaskFailure = [pscustomobject][ordered]@{
+        workerId = $WorkerID
+        errorMessage = $ErrorMessage
+        retries = $Retries
+    } 
+    
+    $CamundaExternalTaskFailure | where {$RetryTimeout} | Add-Member -MemberType NoteProperty -Name retryTimeout -Value $RetryTimeout
+
+    $CamundaExternalTaskFailureJSONParameters = $CamundaExternalTaskFailure | ConvertTo-Json -Depth 10
+
+    Invoke-CamundaRestAPIFunction -MethodPath "/external-task/$ExternalTaskID/failure" -MethodHttpVerb Post -Body $CamundaExternalTaskFailureJSONParameters
+}
+
+function Set-CamundaExternalTaskRetries {
+    param(
+        [parameter(Mandatory,ValueFromPipelineByPropertyName)][alias("id")]$ExternalTaskID,
+        [parameter(Mandatory)]$Retries        
+    )
+
+    $CamundaExternalTaskRetriesJSON = [pscustomobject][ordered]@{
+        retries = $Retries
+    } | ConvertTo-Json -Depth 10
+    
+    Invoke-CamundaRestAPIFunction -MethodPath "/external-task/$ExternalTaskID/retries" -MethodHttpVerb PUT -Body $CamundaExternalTaskRetriesJSON
 }
